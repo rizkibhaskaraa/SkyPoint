0886b06514986af5dcfac4243090939881a4943c
febiolagatha <febiolagatha@gmail.com>
2020-10-09 16:26:14 +0700

    Update

--- application/controllers/App.js
+++ application/controllers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './assets/css/login/login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './controller/home'
 
 import Login from './controller/Login'
 
@@ -12,6 +13,8 @@ class App extends Component {
         <Navbar></Navbar>
 
         <Switch>
+        <Route path='/home' component={Home}></Route>
+
           <div className="App">
             <div className="container">
               <Route exact path="/login" component={Login} />
